91a7f8984003fc9f1655c6c8be6e2d7aa4ba43bd
Matthew Purdon <matthew.purdon@trajectorservices.com>
2025-04-07 23:49:22 -0400

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
+  {
+    // Ignoring variables with leading underscores in the events folder
+    files: ['src/libs/events/custom/**/*.ts'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  tseslint.configs.recommended,
+]);
